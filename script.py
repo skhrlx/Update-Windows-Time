0da82787a3269ca2b4f0ef8c918023d49bf171a6
skhrlx <85849948+skhrlx@users.noreply.github.com>
2023-09-04 21:03:13 -0300

    Update script.py

--- script.py
+++ script.py
@@ -5,9 +5,9 @@ import time
 import datetime
 import win32api
 
-server_list = ['pool.ntp.br', 'time.nist.gov', 'time.windows.com', 'pool.ntp.org']
+server_list = ['time.nist.gov']
 
-def gettime_ntp(addr='pool.ntp.br'):
+def gettime_ntp(addr='time.nist.gov'):
     TIME1970 = 2208988800 
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     data = bytes('\x1b' + 47 * '\0', 'utf-8')
@@ -19,15 +19,15 @@ def gettime_ntp(addr='pool.ntp.br'):
             epoch_time = struct.unpack('!12I', data)[10]
             epoch_time -= TIME1970
             return epoch_time
-    except socket.timeout:
-        return None
+    except Exception as e:
+        print("An error occurred:", str(e))
 
 if __name__ == "__main__":
     for server in server_list:
         epoch_time = gettime_ntp(server)
         if epoch_time is not None:
             utcTime = datetime.datetime.utcfromtimestamp(epoch_time)
-            win32api.SetSystemTime(utcTime.year, utcTime.month, utcTime.day, utcTime.hour, utcTime.minute, utcTime.second, 0)
+            win32api.SetSystemTime(utcTime.year, utcTime.month, utcTime.weekday(), utcTime.day, utcTime.hour, utcTime.minute, utcTime.second, 0)
             localTime = datetime.datetime.fromtimestamp(epoch_time)
             print("Time updated to: " + localTime.strftime("%Y-%m-%d %H:%M") + " from " + server)
             break
